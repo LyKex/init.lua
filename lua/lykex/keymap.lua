906f99b43b9b6d75a9ca912c92453ba41ad34a99
gyliu <liuguoyuan01@outlook.com>
2023-05-12 09:51:12 +0200

    update

--- lua/lykex/keymap.lua
+++ lua/lykex/keymap.lua
@@ -20,3 +20,10 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+-- navigation
+vim.keymap.set("n", "<leader>h", "<C-W>h")
+vim.keymap.set("n", "<leader>j", "<C-W>j")
+vim.keymap.set("n", "<leader>k", "<C-W>k")
+vim.keymap.set("n", "<leader>l", "<C-W>l")
