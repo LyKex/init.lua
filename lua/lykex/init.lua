d63d0997079eed0167dc21409149eb8f01033821
gyliu <liuguoyuan01@outlook.com>
2023-07-26 21:37:27 +0200

    update

--- lua/lykex/init.lua
+++ lua/lykex/init.lua
@@ -236,7 +236,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Setup neovim lua configuration
 require('neodev').setup()
-
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
